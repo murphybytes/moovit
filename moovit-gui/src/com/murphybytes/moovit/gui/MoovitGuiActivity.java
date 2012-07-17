1b4dd0e2cd8f1d0f330c5b01777e3c8e79d9da26
John Murphy <murphybytes@gmail.com>
2012-07-16 23:24:15 -0500

    initial project framework

---
+++ moovit-gui/src/com/murphybytes/moovit/gui/MoovitGuiActivity.java
@@ -0,0 +1,13 @@
+package com.murphybytes.moovit.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoovitGuiActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
